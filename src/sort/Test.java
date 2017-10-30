0c8e5cd11a11d8d3b6f547df6bfd9a76b43cce4f
olehmv <oleg19850405@gmail.com>
2017-10-30 20:41:51 +0200

    test

--- src/sort/Test.java
+++ src/sort/Test.java
@@ -5,240 +5,126 @@ import java.util.Arrays;
 public class Test {
 
 	public static void main(String[] args) {
-		int[] a = { 2, 3, 5, 1, 6, 8, 7, 9, };
-
-		// bublesort(a);
-//		 selectionsort(a);
-		// insertionsort(a);
-		 Arrays.stream(findThreeIndexWithMinValue(a)).forEach(System.out::println);
-		 
-		// fibonacci(8);
-//		System.out.println(multiple(10,20, 30));
-//		System.out.println(lcm(10, 20));
-//		System.out.println(gcd(2, 4));
+		int[] a = { 2, 3, 1, 5, 8, 7, 9, 10, 4, 6 };
+		System.out.println(factorialR(5));
+		System.out.println(Arrays.toString(a));
+		System.out.println(Arrays.toString(find3IndexWithMInV(a)));
+		System.out.println(lcm(a));
 	}
-
-	public static void bublesort(int[] a) {
-		for (int i = 0; i < a.length; i++) {
+	public static void sortB(int[] a) {
+		for (int i = 0; i < a.length - 1; i++) {
 			for (int j = 0; j < a.length - 1 - i; j++) {
-				if (a[j] < a[j + 1]) {
+				if (a[j] > a[j + 1]) {
 					int temp = a[j];
 					a[j] = a[j + 1];
 					a[j + 1] = temp;
 				}
+
 			}
 		}
 	}
-
-	public static void selectionsort(int[] a) {
+	public static void sortS(int a[]) {
 		for (int i = 0; i < a.length; i++) {
-			int max = i;
+			int min = i;
 			for (int j = i + 1; j < a.length; j++) {
-				if (a[j] > a[max]) {
-					max = j;
+				if (a[j] < a[min]) {
+					min = j;
 				}
 			}
 			int temp = a[i];
-			a[i] = a[max];
-			a[max] = temp;
+			a[i] = a[min];
+			a[min] = temp;
 		}
-
 	}
-
-	public static void insertionsort(int[] a) {
+	public static void sortI(int a[]) {
 		for (int i = 1; i < a.length; i++) {
 			int key = a[i];
 			int j = i - 1;
-			while (j >= 0 && a[j] < key) {
+			while (j >= 0 && a[j] > key) {
 				a[j + 1] = a[j];
 				j--;
 			}
 			a[j + 1] = key;
 		}
 	}
-
-	public static int factorial(int n) {
-		int result = 1;
-		while (n > 0) {
-			result = result * n;
-			n--;
-		}
-		return result;
-	}
-
-	public static void fibonacci(int n) {
-		int first = 0;
-		int second = 1;
-		int next = 0;
-		while (n > 0) {
-			System.out.println(next);
-			first = second;
-			second = next;
-			next = first + second;
-			n--;
-		}
-	}
-
-	public static int multiple(int... a) {
-		int smalest = Arrays.stream(a).min().getAsInt();
+	public static int[] find3IndexWithMInV(int a[]) {
+		int firstIndex = 0;
+		int secondIndex = 0;
+		int thirdIndex = 0;
+		int fMin = Integer.MAX_VALUE;
+		int sMin = Integer.MAX_VALUE;
+		int tMin = Integer.MAX_VALUE;
 		for (int i = 0; i < a.length; i++) {
-			smalest = lcm(a[i], smalest);
+			if (a[i] < fMin) {
+				tMin = sMin;
+				sMin = fMin;
+				fMin = a[i];
+				thirdIndex = secondIndex;
+				secondIndex = firstIndex;
+				firstIndex = i;
+			}
+			if (a[i] < sMin && a[i] > fMin) {
+				tMin = sMin;
+				sMin = a[i];
+				thirdIndex = secondIndex;
+				secondIndex = i;
+			}
+			if (a[i] < tMin && a[i] > sMin) {
+				tMin = a[i];
+				thirdIndex = i;
+			}
 		}
-		return smalest;
+		return new int[] { firstIndex, secondIndex, thirdIndex };
 	}
-
-	public static int lcm(int a, int b) {
-		return (a * b) / gcd(a, b);
-	}
-
 	public static int gcd(int a, int b) {
 		if (b == 0)
 			return a;
 		return gcd(b, a % b);
 	}
-	public static int[] findTwoHighest(int[] array)
-	{
-	    int max = Integer.MIN_VALUE;
-	    int secondMax = Integer.MIN_VALUE;
 
-	    for (int value:array)
-	    {
-	        if (value > max)
-	        {
-	            secondMax = max;
-	            max = value;
-	        }
-	        else if (value > secondMax && value < max)
-	        {
-	            secondMax = value;
-	        }
-	    }
-	    return new int[] { max, secondMax };
+	public static int gcd(int a[]) {
+		int result = a[0];
+		for (int n : a)
+			result = gcd(result, n);
+		return result;
 	}
-	public static int[] findThreeHighest(int[] array)
-	{
-	    int max = Integer.MIN_VALUE;
-	    int secondMax = Integer.MIN_VALUE;
-	    int thriedMax = Integer.MIN_VALUE;
-	   
-
-	    for (int value:array)
-	    {
-	        if (value > max)
-	        {
-	        	thriedMax=secondMax;
-	            secondMax = max;
-	            max = value;
-	        }
-	        else if (value > secondMax && value < max)
-	        {
-	        	thriedMax=secondMax;
-	            secondMax = value;
-	        }
-	        else if (value > thriedMax && value < secondMax)
-	        {
-	        	thriedMax = value;
-	        }
-	    }
-	    return new int[] { max, secondMax,thriedMax};
+	public static int  lcm(int a,int b){
+		return (a*b)/gcd(a,b);
 	}
-	
-	public static int[] findTwoLowest(int[] array)
-	{
-	    int max = Integer.MAX_VALUE;
-	    int secondMax = Integer.MAX_VALUE;
-
-	    for (int value:array)
-	    {
-	        if (value < max)
-	        {
-	            secondMax = max;
-	            max = value;
-	        }
-	        else if (value < secondMax && value > max)
-	        {
-	            secondMax = value;
-	        }
-	    }
-	    return new int[] { max, secondMax };
+	public static int lcm(int a[]){
+		int smallest=a[0];
+		for(int n:a)smallest=lcm(smallest,n);
+		return smallest;
 	}
-	public static int [] findTwoIndexWithHeigthValue(int [] a){
-		int firstMax=Integer.MIN_VALUE;
-		int secondMax=Integer.MIN_VALUE;
-		int firstIndex=0;
-		int secondIndex=0;
-		for(int i=0;i<a.length;i++){
-			if(a[i]>firstMax){
-				secondMax=firstMax;
-				firstMax=a[i];
-				secondIndex=firstIndex;
-				firstIndex=i;
-			}else if(a[i]>secondMax&&a[i]<firstMax){
-				secondMax=a[i];
-				secondIndex=i;
-			}
+	public static void fibonacci(int n){
+		int first=0;
+		int second=1;
+		int next=0;
+		while(n>0){
+			System.out.println(next);
+			first=second;
+			second=next;
+			next=first+second;
+			n--;
 		}
-		return new int [] {firstIndex,secondIndex};
 	}
-	public static int [] findThreeIndexWithMaxValue(int [] a){
-		int firstMax=Integer.MIN_VALUE;
-		int secondMax=Integer.MIN_VALUE;
-		int thirdMax=Integer.MIN_VALUE;
-		int firstIndex=0;
-		int secondIndex=0;
-		int thirdIndex=0;
-		for(int i=0;i<a.length;i++){
-			if(a[i]>firstMax){
-				thirdMax=secondMax;
-				secondMax=firstMax;
-				firstMax=a[i];
-				thirdIndex=secondIndex;
-				secondIndex=firstIndex;
-				firstIndex=i;
-				
-			}else if(a[i]>secondMax&&a[i]<firstMax){
-				thirdMax=secondMax;
-				secondMax=a[i];
-				thirdIndex=secondIndex;
-				secondIndex=i;
-				
-			}else if(a[i]>thirdMax&&a[i]<secondMax){
-				thirdMax=a[i];
-				thirdIndex=i;
-			}
+	public static int fibonacciR(int n)  {
+	    if(n == 0)
+	        return 0;
+	     if(n <= 2)
+	      return 1;
+	      return fibonacciR(n - 1) + fibonacciR(n - 2);
+	}
+	public static int factorial(int n){
+		int result=1;
+		while(n>0){
+			result=result*n;
+			n--;
 		}
-		return new int []{firstIndex,secondIndex,thirdIndex};
+		return result;
 	}
-	public static int [] findThreeIndexWithMinValue(int [] a){
-		int firstMin=Integer.MAX_VALUE;
-		int secondMin=Integer.MAX_VALUE;
-		int thirdMin=Integer.MAX_VALUE;
-		int firstIndex=0;
-		int secondIndex=0;
-		int thirdIndex=0;
-		for(int i=0;i<a.length;i++){
-			if(a[i]<firstMin){
-				thirdMin=secondMin;
-				secondMin=firstMin;
-				firstMin=a[i];
-				thirdIndex=secondIndex;
-				secondIndex=firstIndex;
-				firstIndex=i;
-			}else if(a[i]<secondMin&&a[i]>firstMin){
-				thirdMin=secondMin;
-				secondMin=a[i];
-				thirdIndex=secondIndex;
-				secondIndex=i;
-			}else if(a[i]<thirdMin&&a[i]>secondMin){
-				thirdMin=a[i];
-				thirdIndex=i;
-			}
-			
-		}
-		return new int [] {firstIndex,secondIndex,thirdIndex};
-		
+	public static int factorialR(int n){
+		return (n<1)?1:n*factorialR(n-1);
 	}
-	
 
 }
-
